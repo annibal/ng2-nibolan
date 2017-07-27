a2e4756c45ed17fc1d0d4c314fdaca833da7341e
Arthur Annibal <arthur.annibal@gmail.com>
2017-07-26 22:47:51 -0300

    heu

--- app/app.component.js
+++ app/app.component.js
@@ -9,11 +9,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
+        this.message = "Mas que teste de mensagem é esse?";
+        this.items = [
+            "oi",
+            "Olar",
+            "uehu teste 123",
+            "Maisum"
+        ];
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n    <div class=\"jumbotron\">\n      <h1>Welcome to Our App!</h1>\n    </div>\n  ",
+            template: "\n    <header>\n      <nav class=\"navbar navbar-inverse\">\n        <div class=\"navbar-header\">\n          <a href=\"/\" class=\"navbar-brand\">Olar</a>\n        </div>\n      </nav>\n    </header>\n\n    <main>\n      <div class=\"jumbotron\">\n        <h1>Welcome to Our App!</h1>\n        <p>{{message}}</p>\n      </div>\n    </main>\n\n    <ul class=\"list-group\" *ngIf=\"items && items.length > 0\">\n      <li class=\"list-group-item\" *ngFor=\"let item of items\">\n        {{item}}\n      </li>\n    </ul>\n\n    <footer>\n      Coperight &copy; nibalAn\n    </footer>\n\n  ",
             styles: ["\n    .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }\n  "]
         })
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,43 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <div class="jumbotron">
-      <h1>Welcome to Our App!</h1>
-    </div>
+    <header>
+      <nav class="navbar navbar-inverse">
+        <div class="navbar-header">
+          <a href="/" class="navbar-brand">Olar</a>
+        </div>
+      </nav>
+    </header>
+
+    <main>
+      <div class="jumbotron">
+        <h1>Welcome to Our App!</h1>
+        <p>{{message}}</p>
+      </div>
+    </main>
+
+    <ul class="list-group" *ngIf="items && items.length > 0">
+      <li class="list-group-item" *ngFor="let item of items">
+        {{item}}
+      </li>
+    </ul>
+
+    <footer>
+      Coperight &copy; nibalAn
+    </footer>
+
   `,
   styles: [`
     .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }
   `]
 })
-export class AppComponent {}
+export class AppComponent {
+  message = "Mas que teste de mensagem é esse?";
+  items = [
+    "oi",
+    "Olar",
+    "uehu teste 123",
+    "Maisum"
+  ];
+
+}
